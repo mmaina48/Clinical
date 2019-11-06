d000d19fe06c644a4ffc727ece8543ca7f5995a5
mmaina48 <mmaina48@gmail.com>
2019-11-06 20:44:37 +0300

    add wtform

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask ,render_template
+from wtform_fields import *
 
+#config app 
 app=Flask(__name__)
 app.secret_key='maich key'
 
 @app.route('/',methods=['GET', 'POST'])
-def login():
- 
-    return render_template("home.html")
+def reg():
+
+    reg_form=RegistrationForm()
+    if reg_form.validate_on_submit():
+        return "Great Sucess!"
+
+    return render_template("home.html", form=reg_form)
 
 if __name__=="__main__":
     app.run(debug=True)
