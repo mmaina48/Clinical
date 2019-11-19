815d0b46ca073185ae474291b3703ab7022447fa
mmaina48 <mmaina48@gmail.com>
2019-11-19 17:49:48 +0300

    added login page and login validation

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask ,render_template
+from flask import Flask, render_template,redirect,url_for
 from wtform_fields import *
 from models import *
 
@@ -14,29 +14,38 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://hohloipsofwxru:c3f564adeb73c
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+ 
 db=SQLAlchemy(app)
 
-@app.route('/',methods=['GET', 'POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def reg():
 
     reg_form=RegistrationForm()
+#update database if validation sucessfull
     if reg_form.validate_on_submit():
         username = reg_form.username.data
         password = reg_form.password.data
 
-
-        #check username is unique
-        user_object= User.query.filter_by(username=username).first()
-        if user_object:
-            return "Someone else has taken this username!"
-
-        user= User(username=username, password=password)
+        user = User(username=username, password=password)
         db.session.add(user)
         db.session.commit()
-        return "Inserted to db"            
+
+        return redirect(url_for('login'))        
 
     return render_template("home.html", form=reg_form)
 
+@app.route("/login", methods=['GET','POST'])
+def login():
+    #initatiate
+    loginform = LoginForm()
 
-if __name__=="__main__":
+    #allow login if no validation error
+    if loginform.validate_on_submit():
+        return "Logged in successfully"
+
+    return render_template("login.html", form=loginform)
+
+
+if __name__== "__main__":
     app.run(debug=True)
--- wtform_fields.py
+++ wtform_fields.py
@@ -2,6 +2,20 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, EqualTo, ValidationError
 
+from models import User
+
+def invalid_credentials(form,field):
+    """ Username and password checker"""
+    username_entered = form.username.data
+    password_entered = field.data
+
+    #check credentials if exixt
+    user_object = User.query.filter_by(username=username_entered).first()
+    if user_object is None:
+        raise ValidationError("Username or Password is Invalid")
+
+    elif password_entered != user_object.password:
+        raise ValidationError("Username or Password is Invalid")
 
 
 class RegistrationForm(FlaskForm):
@@ -11,3 +25,14 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('password', validators=[InputRequired(message="Password required"), Length(min=4, max=25, message="Password must be between 4 and 25 characters")])
     confirm_pswd = PasswordField('confirm_pswd', validators=[InputRequired(message="Password required"), EqualTo('password', message="Passwords must match")])
     submit_button = SubmitField('Create')
+
+    def validate_username(self, username):
+        user_object = User.query.filter_by(username=username.data).first()
+        if user_object:
+            raise ValidationError("Username already exists. Select a different username.")
+
+class LoginForm(FlaskForm):
+    """ login form """
+    username = StringField('username_label', validators=[InputRequired(message="Username required")])
+    password = PasswordField('password_label', validators=[InputRequired(message="Username required"),invalid_credentials])
+    submit_button = SubmitField('Login')
